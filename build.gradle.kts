a686efdd76f2c1c8ff70a9274c37a01d6e70c15d
Asja <anastasjamedved@gmail.com>
2025-05-20 19:25:36 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    java
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation("org.seleniumhq.selenium:selenium-java:4.29.0")
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.9.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    systemProperty("chromeoptions.prefs", System.getProperty("chromeoptions.prefs", "profile.password_manager_leak_detection=false"))
+}
